7dfbc7756d43040ae5374727dc985ae8d3530a8c
Austin Hyder <amhyder5@gmail.com>
2015-03-20 22:08:28 -0400

    Init Fix

--- src/org/usfirst/frc/team3229/robot/Robot.java
+++ src/org/usfirst/frc/team3229/robot/Robot.java
@@ -11,15 +11,16 @@ import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 
-/**
- * This is a demo program showing how to use Mecanum control with the RobotDrive class.
- */
+
 public class Robot extends SampleRobot {
 	
     RobotDrive robotDrive;
     Joystick stick;
     SpeedController elevatorMotor;
-    
+    SpeedController FrontRight;
+    SpeedController BackRight;
+    SpeedController FrontLeft;
+    SpeedController BackLeft;
     // Channels for the wheels
     final int frontLeftChannel	= 3;       //originally 3
     final int rearLeftChannel	= 4;       //originally 4
@@ -28,12 +29,11 @@ public class Robot extends SampleRobot {
     final int elevator = 6;
     private static final int LIFT_BUTTON=2; //or whatever button raises
     private static final int LOWER_BUTTON=1; //or whatever button lowers
-    private static final int ROTATE_LEFT=4; 
-    private static final int ROTATE_RIGHT=5;
+
     
     // The channel on the driver station that the joystick is connected to
     final int joystickChannel	= 0;
-    int rotationDirection;
+    double rotationDirection;
 
     public Robot() {
         robotDrive = new RobotDrive(frontLeftChannel, rearLeftChannel, frontRightChannel, rearRightChannel);
@@ -52,27 +52,74 @@ public class Robot extends SampleRobot {
     /**
      * Runs the motors with Mecanum drive.
      */
+    public void autonomous(){
+    	
+    	for(int i = 0; i < 400; i++){
+    		if(isAutonomous() && isEnabled() ){
+    		
+    			
+    			robotDrive.setInvertedMotor(MotorType.kFrontRight, false);
+    			robotDrive.setInvertedMotor(MotorType.kRearLeft, true);
+    			
+    			robotDrive.mecanumDrive_Cartesian(0, 0.5, 0, 1);
+    			Timer.delay(.005);
+    			robotDrive.mecanumDrive_Cartesian(0, 0, 0, 1);} 
+    			robotDrive.setInvertedMotor(MotorType.kRearLeft, false);
+    			robotDrive.setInvertedMotor(MotorType.kFrontRight,  true);
+    	}
+    	
+    }
+    
     public void operatorControl() {
         robotDrive.setSafetyEnabled(true);
         while (isOperatorControl() && isEnabled()) {
-        	if(stick.getRawButton(ROTATE_RIGHT)) {
-        			rotationDirection = 1;
-        		}
-        	else{
-        		
-        	}
-        	if(stick.getRawButton(ROTATE_LEFT)) {
-        			rotationDirection = -1;
-        	}
-        	else{
-        		rotationDirection=0;
-        	}
-        	
-        	
+      
+   
         	// Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
         	// This sample does not use field-oriented drive, so the gyro input is set to zero.
-            robotDrive.mecanumDrive_Cartesian(-stick.getY(), -stick.getX(), rotationDirection, 1);
+        	
+           
+           
+          //******USED FOR DEBUG (CHECK RIOLOG*******
+            System.out.println(-stick.getRawAxis(2));
+            System.out.println(stick.getRawAxis(3));
+          //*****************************************
             
+        double RotateStuff = 0;
+        		
+         //
+         if (-stick.getRawAxis(2) + stick.getRawAxis(3)>0 & RotateStuff == 0){
+          	  robotDrive.setInvertedMotor(MotorType.kFrontLeft, false);
+          	  robotDrive.setInvertedMotor(MotorType.kFrontRight, false);																// invert the left side motors
+            RotateStuff= 1;
+          }
+          
+        if (-stick.getRawAxis(2) + stick.getRawAxis(3)<0 & RotateStuff==0){
+        	  	// invert the left side motors
+        	  robotDrive.setInvertedMotor(MotorType.kFrontRight, false);
+        	  robotDrive.setInvertedMotor(MotorType.kFrontLeft, true);
+        	  robotDrive.setInvertedMotor(MotorType.kRearLeft, true);
+        	  RotateStuff = 2;
+          }
+        /* Basic Movement Code */robotDrive.mecanumDrive_Cartesian(-0.8 * stick.getY(), -0.8 * stick.getX(), -0.8 * stick.getRawAxis(2) + 0.8 * stick.getRawAxis(3), 1);
+        
+        /*  if (RotateStuff == 1){
+          	RotateStuff = 0;
+          	robotDrive.setInvertedMotor(MotorType.kFrontLeft,  false);
+          	robotDrive.setInvertedMotor(MotorType.kFrontRight, true);	
+          }
+          else if (RotateStuff == 2){
+        	 robotDrive.setInvertedMotor(MotorType.kFrontRight, true);
+        	 robotDrive.setInvertedMotor(MotorType.kFrontLeft, false);
+        	 
+        	  RotateStuff = 0;
+          }*/
+
+          robotDrive.setInvertedMotor(MotorType.kFrontLeft, true);	// invert the left side motors
+      	  robotDrive.setInvertedMotor(MotorType.kFrontRight, true);
+      	  robotDrive.setInvertedMotor(MotorType.kRearLeft, false);	// invert the left side motors
+    	  robotDrive.setInvertedMotor(MotorType.kRearRight, false);
+         //***************START OF ELEVATOR CODE********************** 
             if(stick.getRawButton(LIFT_BUTTON)) {
                 elevatorMotor.set(0.5);
            } else if(stick.getRawButton(LOWER_BUTTON)) {
@@ -80,6 +127,7 @@ public class Robot extends SampleRobot {
            } else {
                 elevatorMotor.set(0);
            }
+          //**********************************************************   
             
             Timer.delay(0.010);	// wait 5ms to avoid hogging CPU cycles
         }
@@ -90,6 +138,3 @@ public class Robot extends SampleRobot {
 		// TODO Auto-generated method stub
 		
 	}
-    	
-    
-
